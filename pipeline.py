6240aca4dcf1a44e5849db76b4ff58ba7ca0e08a
benebeau214 <kimrn142@gmail.com>
2026-09-18 15:11:00 +0900

    feat : 데이터 로드/인코딩 해결

---
+++ pipeline.py
@@ -0,0 +1,26 @@
+import csv
+import pandas as pd
+
+df = pd.read_csv("RAW_DATA.csv", encoding="cp949")
+
+print(df.shape)
+print(df.info())
+
+'''
+df.shape, df.info() 출력 결과
+
+(500, 5)
+<class 'pandas.DataFrame'>
+RangeIndex: 500 entries, 0 to 499
+Data columns (total 5 columns):
+ #   Column  Non-Null Count  Dtype
+---  ------  --------------  -----
+ 0   주문일자    500 non-null    str  
+ 1   상품명     500 non-null    str  
+ 2   카테고리    500 non-null    str  
+ 3   단가      500 non-null    str  
+ 4   수량      500 non-null    int64
+dtypes: int64(1), str(4)
+memory usage: 19.7 KB
+None
+'''
